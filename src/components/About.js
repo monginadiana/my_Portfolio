6558dadb915c3c2e144eb06d6f184cbc7e774729
monginadiana <dianahmongina2@gmail.com>
2023-01-26 22:40:20 +0300

    created the about component with  my skills and an about me paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -1,146 +1,99 @@
 import { Link } from "react-router-dom";
 
-export default function About(){
-    return(
-        <>
-            <section className="intro text-center section-padding color-bg" id="about">
-                <center>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-1 col-md-offset-2 wp1"></div>
-                            <div className="col-10 col-md-offset-2 wp1 my-5">
-                                <h1 className="arrow">A little <span>about</span> me</h1>
-                                <p>
-                                I am a Software Engineer with 2+ years of experience developing
-                                awesome web applications. I primarily tend to lean towards using
-                                Django and React for my web applications and I enjoy coding all the
-                                time. Am also a passionate technical instructor and writer with the aim
-                                of supporting the next generation of techies. Moreover, I've worked as
-                                a team lead in various projects, am a team player and a fast learner
-                                very flexible to work as a frontend or backend developer.
-                                   </p>
-                            <Link to="/resume" className="btn btn-outline-light mt-3">Resume</Link>
-                            </div>
-                            <div className="col-1 col-md-offset-2 wp1"></div>
+export default function About() {
+  return (
+    <>
+      <section
+        className="intro text-center section-padding color-bg"
+        id="about"
+      >
+        <center>
+          <div className="container">
+            <div className="row">
+              <div className="col-1 col-md-offset-2 wp1"></div>
+              <div className="col-10 col-md-offset-2 wp1 my-5">
+                <h1 className="arrow">
+                  About Diana
+                </h1>
+                <p>
+                  Experienced software engineer with 2+ years of experience and
+                  a passion for developing innovative programs that expedite the
+                  efficiency and effectiveness of organizational success.
+                  Well-versed in technology and writing code to create systems
+                  that are reliable and user-friendly. Skilled leader who has
+                  the proven ability to motivate, educate, and manage a team of
+                  professionals to build software programs and effectively track
+                  changes. Confident communicator, strategic thinker, and
+                  innovative creator.
+                </p>
+                <Link to="/resume" className="btn btn-outline-light mt-3">
+                  Resume
+                </Link>
+              </div>
+              <div className="col-1 col-md-offset-2 wp1"></div>
+            </div>
+          </div>
+        </center>
+      </section>
+      <div className="container">
+        <center>
+          <h2 className="skills my-3">Skills</h2>
+        </center>
 
-                        </div>
-                    </div>
-                </center>
-            </section>
-                <div className="container">
-                    <center>
-                        <h2 className="skills my-3">Skills</h2>
-                    </center>
+        <div class=" wow fadeInUp container-skillbar" data-wow-delay="0.2s">
+          <div class="skillbar clearfix " data-mdb-percent="90%">
+            <div class="skillbar-title" style={{background: "#218559"}}><span>HTML5</span></div>
+            <div class="skillbar-bar" style={{background: "#218559", width:"90%"}}></div>
+            <div class="skill-bar-percent">90%</div>
+          </div>
 
-                    <div className="skills-main">
-                    <div className="tools">
-                    <h4 className="skill-header">AWS</h4>
-                    <div className="side-line">
-                        <p className="skill-name">Serverless</p>
-                    </div>
-                    <div className="side-line">
-                        <p className="skill-name">Docker</p>
-                    </div>
-                    <div className="side-line">
-                        <p className="skill-name">Kubernetes</p>
-                    </div>
-                    <div className="side-line">
-                        <p className="skill-name">S3 Buckets</p>
-                    </div>
-                    <div className="side-line pb-0">
-                        <p className="skill-name">Lambda</p>
-                    </div>
-   
-                   </div>
+          <div class="skillbar clearfix " data-percent="90%">
+            <div class="skillbar-title" style={{background: "#EBB035"}}><span>CSS3</span></div>
+            <div class="skillbar-bar" style={{background: "#EBB035" , width:"90%"}}></div>
+            <div class="skill-bar-percent">90%</div>
+          </div>
 
-                       <div className="javascript">
-                        <h4 className="skill-header">Javascript</h4>
-                        <div className="side-line">
-                            <p className="skill-name">React</p>
-                        </div>
-                        <div className="side-line">
-                            <p className="skill-name">Angular</p>
-                        </div>
-                        <div className="side-line">
-                            <p className="skill-name">JQuery</p>
-                        </div>
-                        <div className="side-line">
-                            <p className="skill-name">NodeJS</p>
-                        </div>
-                        <div className="side-line pb-0">
-                            <p className="skill-name">AJAX</p>
-                        </div>
-                       </div>
+          <div class="skillbar clearfix " data-percent="88%">
+            <div class="skillbar-title" style={{background: "#332D2D"}}><span>Tailwind Css</span></div>
+            <div class="skillbar-bar" style={{background: "#332D2D", width:"88%"}}></div>
+            <div class="skill-bar-percent">88%</div>
+          </div> 
 
-                       <div className="python">
-                        <h4 className="skill-header">Python</h4>
-                        <div className="side-line">
-                            <p className="skill-name">Django</p>
-                        </div>
-                        <div className="side-line">
-                            <p className="skill-name">Django REST</p>
-                        </div>
-                        <div className="side-line pb-0">
-                            <p className="skill-name">Flask</p>
-                        </div>
+          <div class="skillbar clearfix " data-percent="85%">
+            <div class="skillbar-title" style={{background: "#6840D4"}}><span>React Js</span></div>
+            <div class="skillbar-bar" style={{background: "#6840D4",width:"85%"}}></div>
+            <div class="skill-bar-percent">85%</div>
+          </div> 
 
-                        <div className="ruby">
-                        <h4 className="skill-header">Ruby</h4>
-                        <div className="side-line pb-0">
-                            <p className="skill-name">Ruby on Rails</p>
-                        </div>
-                       </div>
-                       </div>
+          <div class="skillbar clearfix " data-percent="82%">
+            <div class="skillbar-title" style={{background: "#3B71CA"}}><span>Rails</span></div>
+            <div class="skillbar-bar" style={{background: "#3B71CA", width:"84%"}}></div>
+            <div class="skill-bar-percent">82%</div>
+          </div> 
 
-                       <div className="add-ons">
-                        <h4 className="skill-header">HTML</h4>
-                        <div className="side-line">
-                            <p className="skill-name">CSS</p>
-                        </div>
-                        <div className="side-line">
-                            <p className="skill-name">Bootstrap</p>
-                        </div>
-                        <div className="side-line">
-                            <p className="skill-name">MDBootstrap</p>
-                        </div>
-                        <div className="side-line pb-0">
-                            <p className="skill-name">Tailwind</p>
-                        </div>
-                       </div>
+          <div class="skillbar clearfix " data-percent="70%">
+            <div class="skillbar-title" style={{background: "#06A2CB"}}><span>Django</span></div>
+            <div class="skillbar-bar" style={{background: "#06A2CB", width:"70%"}}></div>
+            <div class="skill-bar-percent">70%</div>
+          </div> 
+          <div class="skillbar clearfix " data-percent="70%">
+            <div class="skillbar-title" style={{background: "#DD1E2F"}}><span>Javascript</span></div>
+            <div class="skillbar-bar" style={{background: "#DD1E2F", width:"70%"}}></div>
+            <div class="skill-bar-percent">70%</div>
+          </div> 
+          <div class="skillbar clearfix " data-percent="55%">
+            <div class="skillbar-title" style={{background: "#DD1E2F"}}><span>Docker</span></div>
+            <div class="skillbar-bar" style={{background: "#DD1E2F", width:"55%"}}></div>
+            <div class="skill-bar-percent">55%</div>
+          </div> 
+          <div class="skillbar clearfix " data-percent="45%">
+            <div class="skillbar-title" style={{background: "#DD1E2F"}}><span>Kubernetes</span></div>
+            <div class="skillbar-bar" style={{background: "#DD1E2F", width:"45%"}}></div>
+            <div class="skill-bar-percent">45%</div>
+          </div> 
+        </div>
+      </div>
 
-                       <div className="hosting">
-                        <h4 className="skill-header">Heroku</h4>
-                        <div className="side-line">
-                            <p className="skill-name">Netlify</p>
-                        </div>
-                        <div className="side-line">
-                            <p className="skill-name">Vercel</p>
-                        </div>
-                        <div className="side-line">
-                            <p className="skill-name">Render</p>
-                        </div>
-                        <div className="side-line pb-0">
-                            <p className="skill-name">Github</p>
-                        </div>
-                       </div>
-
-                       <div className="databases">
-                        <h4 className="skill-header">PostgreSQL</h4>
-                        <div className="side-line">
-                            <p className="skill-name">DynamoDB</p>
-                        </div>
-                        <div className="side-line">
-                            <p className="skill-name">SQLite</p>
-                        </div>
-                        <div className="side-line pb-0">
-                            <p className="skill-name">MySQL</p>
-                        </div>
-                       </div>
-
-                    </div>
-
-                </div>
-        
-        </>
-    )
+    </>
+  );
 }
